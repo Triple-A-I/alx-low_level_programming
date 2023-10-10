dac83617ec41e7f74a58a5c8e01b2c2cf9550ac5
Beebo <Albraaengineer@gmail.com>
2023-10-09 22:19:13 -0700

    ..

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Description: ikjhjkh
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+        char ch = 'a';
+	char CH = 'A';
+        while (ch <= 'z')
+        {
+                putchar(ch);
+                ch++;
+        }
+        putchar('\n');
+	while (CH <= 'Z')
+        {
+                putchar(CH);
+                CH++;
+        }
+        putchar('\n');
+        return (0);
+}
